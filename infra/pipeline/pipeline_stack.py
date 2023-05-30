05ab5d2335b2fe159a3aca290fff3ce84c4c2485
hanegraaff <hanegraaff@gmail.com>
2023-05-29 20:32:05 -0400

    wip

--- infra/pipeline/pipeline_stack.py
+++ infra/pipeline/pipeline_stack.py
@@ -2,11 +2,21 @@ import aws_cdk as cdk
 from constructs import Construct
 from aws_cdk.pipelines import CodePipeline, CodePipelineSource, ShellStep
 from pipeline.infra_stage import InfraStage
+from aws_cdk.aws_lambda import AssetCode
+from aws_cdk import BundlingOptions, DockerImage
 
 class MyPipelineStack(cdk.Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-        
+
+        backend_package = AssetCode.from_asset(
+            "backend.zip",
+            bundling=BundlingOptions(
+                image=DockerImage.from_registry("alpine"),
+                command=["sh", "-c", "pwd"]
+            )
+        )
+
 
         pipeline =  CodePipeline(self, "Pipeline",
                         pipeline_name="EmailAssistantPipeline",
