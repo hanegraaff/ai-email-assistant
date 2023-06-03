5439bca92e57408aba5b264de0465abe98738181
hanegraaff <hanegraaff@gmail.com>
2023-06-03 19:58:43 -0400

    wip

--- infra/backend/infra_stack.py
+++ infra/backend/infra_stack.py
@@ -18,7 +18,7 @@ class InfraStack(Stack):
         lambda_function = aws_lambda.Function(
             self, "emailassistant-backend-test",
             runtime=aws_lambda.Runtime.PYTHON_3_8,
-            handler="index.handler",
+            handler=backend_package.path + ".lambda_handler",
             code=backend_package,
             role=role
         )
