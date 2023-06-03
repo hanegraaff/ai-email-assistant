1a157d4eb5747e62cb950f83d414e543684b849a
hanegraaff <hanegraaff@gmail.com>
2023-06-03 17:13:54 -0400

    wip

--- infra/pipeline/assets.py
+++ infra/pipeline/assets.py
@@ -3,9 +3,7 @@ from aws_cdk.aws_lambda import AssetCode
 from aws_cdk.aws_lambda import AssetCode
 from aws_cdk import BundlingOptions, DockerImage
 
-backend_package = None
-'''
+
 backend_package = AssetCode.from_asset(
             "../application_source/target/test-handlers.zip",
         )
-'''
