f8cb7b4e63fd743a031acf51fadbbf77bbd46f14
hanegraaff <hanegraaff@gmail.com>
2023-06-17 12:02:46 -0400

    wip

--- infra/backend/infra_stack.py
+++ infra/backend/infra_stack.py
@@ -94,6 +94,7 @@ class InfraStack(Stack):
         
         record = aws_route53.ARecord(self, "alias_record",
             zone=hosted_zone,
+            record_name="www",
             target=aws_route53.RecordTarget(
                 alias_target=aws_route53_targets.ApiGatewayDomain(custom_domain_name)
             )
