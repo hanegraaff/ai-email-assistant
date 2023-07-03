41464e54594787033a2e68b18ea07ac013b1cb0f
hanegraaff <hanegraaff@gmail.com>
2023-07-03 16:27:33 -0400

    wip

--- application_source/static_content/src/App.js
+++ application_source/static_content/src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [result, setResult] = useState('');
+  const [banner, setBanner] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const callApi = async () => {
+    const response = await fetch('https://api-prod.hal-9001.com/test-data');
+    const data = await response.json();
+    setResult(JSON.stringify(data, null, 2));
+  }
+
+  useEffect(() => {
+    const messages = ["Beware", "Go Away", "You shouldn't be here", "System Failure", "Don't do this", "It's all your fault", "You will regret it"];
+    const bannerMessage = messages[Math.floor(Math.random() * messages.length)];
+    setBanner(bannerMessage);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="container">
+      <div className="banner" onClick={callApi}>{banner}</div>
+      
+      <input
+        className="input"
+        placeholder="Username"
+        style={{color: 'rgba(120, 0, 0, 1)'}}
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      
+      <input
+        className="input"
+        placeholder="Password"
+        style={{color: 'rgba(120, 0, 0, 1)'}}
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <div className="googleLogin">
+        <a className="link" href="https://accounts.google.com/o/oauth2/v2/auth?client_id=CLIENT_ID&redirect_uri=https://localhost:80&response_type=code&scope=https://www.googleapis.com/auth/userinfo.profile&access_type=offline&prompt=consent">
+          Google Login
         </a>
-      </header>
+      </div>
+
+      <div className="results">{result}</div>
     </div>
   );
 }
--- infra/pipeline/assets.py
+++ infra/pipeline/assets.py
@@ -4,16 +4,17 @@ from aws_cdk import Tags
 
 component_name = "Application Bakend"
 
-frontend_package = AssetCode.from_asset(
+'''frontend_package = AssetCode.from_asset(
             "../application_source/frontend/target/frontend_application.zip",
         )
+        
+    Tags.of(frontend_package).add("component_name", component_name)
+    Tags.of(frontend_package).add("Name", "FrontendServiceAsset")
+'''
 
 backend_package = AssetCode.from_asset(
             "../application_source/backend/target/handlers.zip",
         )
-
-Tags.of(frontend_package).add("component_name", component_name)
-Tags.of(frontend_package).add("Name", "FrontendServiceAsset")
 
 Tags.of(backend_package).add("component_name", component_name)
 Tags.of(backend_package).add("Name", "BackendServiceAsset")
